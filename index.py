e403eea9dbb7cea9ba87b3a26c6b51f34d9b5cdb
Bernardo Anderson <bernardodsanderson@gmail.com>
2019-01-12 14:32:54 -0600

    Update to work with query params

--- index.py
+++ index.py
@@ -1,14 +1,19 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
 from justwatch import JustWatch
 import json
-
-just_watch= JustWatch(country='US')
-
-results = just_watch.search_for_item(providers=['mbi'])
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        qs = parse_qs(urlparse(self.path).query)
+        country = qs["country"]
+        if country and len(country[0]) > 1:
+            justwatch_country = country[0]
+        else:
+            justwatch_country = 'US'
+        just_watch = JustWatch(country=justwatch_country)
+        results = just_watch.search_for_item(providers=['mbi'])
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
